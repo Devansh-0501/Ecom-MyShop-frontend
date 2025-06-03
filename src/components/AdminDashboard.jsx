3c2204c4607c1999a85701594a8f2fa8263f856e
DEVANSH-0501 <devansh055leo@gmail.com>
2025-06-03 16:05:05 +0530

    Admin functionalities 1

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import "../styles/AdminDashboard.css";
+import "../styles/adminDashboard.css";
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import ProductCard from './ProductCard';
